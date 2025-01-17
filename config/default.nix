7a3d6657e59f78fdd04c0bbed0e5459c969050ad
Antonio Drumond <adcdstom@gmail.com>
2025-01-17 12:54:16 -0300

    default.nix

---
+++ config/default.nix
@@ -0,0 +1,43 @@
+{ config, lib, ... }:
+{
+  imports = [
+	];
+  options = {
+    theme = lib.mkOption {
+      default = lib.mkDefault "paradise";
+      type = lib.types.enum [
+        "aquarium"
+        "decay"
+        "edge-dark"
+        "everblush"
+        "everforest"
+        "far"
+        "gruvbox"
+        "jellybeans"
+        "material"
+        "material-darker"
+        "mountain"
+        "ocean"
+        "oxocarbon"
+        "paradise"
+        "test"
+        "tokyonight"
+        "yoru"
+      ];
+    };
+    assistant = lib.mkOption {
+      default = "none";
+      type = lib.types.enum [
+        "copilot"
+        "none"
+      ];
+    };
+  };
+  config = {
+    # The base16 theme to use, if you want to use another theme, change it in colorscheme.nix
+    theme = "tokyonight";
+    extraConfigLua = ''
+      _G.theme = "${config.theme}"
+    '';
+  };
+}
