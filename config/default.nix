5bdf85906323bf258b5fed9fba6993ec6ac790cb
Antonio Drumond <adcdstom@gmail.com>
2025-01-21 14:31:57 -0300

    change theme

--- config/default.nix
+++ config/default.nix
@@ -78,7 +78,7 @@
   };
   config = {
     # The base16 theme to use, if you want to use another theme, change it in colorscheme.nix
-    theme = "mountain";
+    theme = "ocean";
     extraConfigLua = ''
       _G.theme = "${config.theme}"
     '';
